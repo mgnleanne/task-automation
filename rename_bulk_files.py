0fd9f4ce464846d9be215ee2ac18da6d03bc5353
Megan Clarke <megan-clarke.com>
2025-05-19 14:43:04 -0400

    Add in comments and code cleanup

--- rename_bulk_files.py
+++ rename_bulk_files.py
@@ -5,11 +5,12 @@ folder = os.path.dirname(os.path.abspath(__file__))
 
 for filename in os.listdir(folder):
     if filename.endswith('.txt'):
-        # print(f"Found text file: {filename}")
+        # Modify file name (change _backup string as needed)
         old_path = os.path.join(folder, filename)
         new_name = filename.replace('.txt','_backup.txt')
         new_path = os.path.join(folder, new_name)
         os.rename(old_path, new_path)
 
+# Prints out all files in dir to the console
 for filename in os.listdir(folder):
         print(filename)
