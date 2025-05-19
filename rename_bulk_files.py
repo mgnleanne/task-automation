0ca5c312ec8753ba13af485315bae02579ce0678
Megan Clarke <megan-clarke.com>
2025-05-19 14:37:15 -0400

    Add file renaming script

---
+++ rename_bulk_files.py
@@ -0,0 +1,13 @@
+import os
+
+folder = 'C:\\Users\\Megan\\Documents\\Coding'
+for filename in os.listdir(folder):
+    if filename.endswith('.txt'):
+        # print(f"Found text file: {filename}")
+        old_path = os.path.join(folder, filename)
+        new_name = filename.replace('.txt','_backup.txt')
+        new_path = os.path.join(folder, new_name)
+        os.rename(old_path, new_path)
+
+for filename in os.listdir(folder):
+        print(filename)
